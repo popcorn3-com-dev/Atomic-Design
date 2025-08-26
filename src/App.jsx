56b28e4ee230cf7629067851f1cdd262e49b9954
popcorn3-com-dev <shos3104@gmail.com>
2025-08-26 16:11:37 +0900

    organisms

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,18 @@ import './App.css';
 import { PrimaryButton } from './components/atoms/button/PrimaryButton';
 import { SecondaryButton } from './components/atoms/button/SecondaryButton';
 import { SearchInput } from './components/molecules/SearchInput';
+import { UserCard } from './components/organisms/user/UserCard';
+
+const user = {
+  name: "ゆうた",
+  image: "https://plus.unsplash.com/premium_photo-1676389281733-aaefab0e7907",
+  email: "aaaa@aaaa.com",
+  phone: "000-999-9999",
+  company: {
+    name: "鈴木商事"
+  },
+  website: "https://google.com"
+};
 
 function App() {
   return (
@@ -10,6 +22,7 @@ function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </>
   );
 }
