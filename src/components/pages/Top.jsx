b0e5899d2ba22d3453cfbe5ab0860d2a20f8f65f
popcorn3-com-dev <shos3104@gmail.com>
2025-10-07 18:13:22 +0900

    Atomic design

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 import { Header } from "../atoms/layout/Header";
+import { Footer } from "../atoms/layout/Footer";
 
 export const Top = () => {
     return (
         <>
             <Header />
             <SH2>TOPページ</SH2>
+            <Footer />
         </>
     )
 }
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -4,32 +4,46 @@ import { Footer } from "../atoms/layout/Footer";
 import { UserCard } from "../organisms/user/UserCard";
 import { SearchInput } from "../molecules/SearchInput";
 
-
-
-const user = {
-    name: "ゆうた",
-    image: "https://plus.unsplash.com/premium_photo-1676389281733-aaefab0e7907",
-    email: "aaaa@aaaa.com",
-    phone: "000-999-9999",
-    company: {
-        name: "鈴木商事"
-    },
-    website: "https://google.com"
-};
+const users = [...Array(10).keys()].map((val) => {
+    return {
+        id: val,
+        name: `ゆうた${val}`,
+        image: "https://plus.unsplash.com/premium_photo-1676389281733-aaefab0e7907",
+        email: "aaaa@aaaa.com",
+        phone: "000-999-9999",
+        company: {
+            name: "鈴木商事"
+        },
+        website: "https://google.com"
+    }
+})
 
 export const Users = () => {
     return (
         <>
             <Header />
-            <SH2>ユーザー一覧</SH2>
-            <SSeach>
-                <SearchInput />
-            </SSeach>
-            <UserCard user={user} />
+            <SContainer>
+                <SH2>ユーザー一覧</SH2>
+                <SSeach>
+                    <SearchInput />
+                </SSeach>
+                <SUserArea>
+                    {users.map((user) => (
+                        <UserCard key={user.id} user={user} />
+                    ))}
+                </SUserArea>
+            </SContainer>
             <Footer />
         </>
     )
 }
+
+const SContainer = styled.div`
+display:flex;
+flex-direction:column;
+align-items:center;
+padding:24px;
+`
 
 const SSeach = styled.div`
 display:flex;
@@ -40,4 +54,11 @@ padding:24px;
 
 const SH2 = styled.h2`
 text-align:center;
+`
+
+const SUserArea = styled.div`
+width:100%;
+display:grid;
+grid-template-columns:repeat(auto-fit, minmax(200px, 1fr));
+grid-gap:20px;
 `
